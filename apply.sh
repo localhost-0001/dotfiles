25295e4fa5ca54bcb952d44d43e5dc7bcdc8a148
Henry Hoff <murp8752@gmail.com>
2021-08-16 16:05:22 -0400

    finished firefox config and setup script

---
+++ apply.sh
@@ -0,0 +1,8 @@
+mkdir ~/.mozilla/firefox/default
+cp ./firefox/user.js       ~/.mozilla/firefox/default
+cp ./firefox/profiles.ini  ~/.mozilla/firefox
+
+cp ./minecraft/options.txt ~/.minecraft
+cp ./minecraft/swords      ~/.minecraft/resourcepacks
+
+cp ./vscode/settings.json  ~/.config/Code/User/settings.json
--- update-dotfiles.sh
+++
@@ -1,4 +0,0 @@
-rm -r ~/.mozilla/firefox/*
-mkdir ~/.mozilla/firefox/default
-cp ./firefox/user.js      ~/.mozilla/firefox/default
-cp ./firefox/profiles.ini ~/.mozilla/firefox
