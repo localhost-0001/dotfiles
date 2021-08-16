7b8ddb3f6d0d4faca7da44437e17c416d63d2e11
Henry Hoff <murp8752@gmail.com>
2021-08-16 12:41:31 -0400

    tweaked some errors

---
+++ configuration.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./vscode.nix
+    ./home.nix
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Set timezone
+  time.timeZone = "America/New_York";
+
+  # Enables UEFI boot
+  boot.loader.systemd-boot.enable = true;
+
+  # Enabling GNOME
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  # Disable default GNOME apps
+  services.gnome.core-utilities.enable = false;
+
+  # Defining users
+  users.users.henry = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  # Package management
+  environment.systemPackages = with pkgs; 
+  [
+    firefox spotify discord
+    alacritty
+    git gcmcore
+    minecraft multimc
+  ];
+
+  # I don't really have much to do with my HDD so I mount it to its own directory
+  fileSystems."/home/henry/hdd" = { 
+    device = "/dev/disk/by-label/hdd";
+    fsType = "ext4";
+  };
+}
---
+++ home.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchGit {
+    url = "https://github.com/rycee/home-manager.git";
+    rev = "b39647e52ed3c0b989e9d5c965e598ae4c38d7ef"; # the commit to fetch 
+    ref = "release-21.05"; # the branch to follow: release-xx.yy for stable nixos or master for nixos-unstable.
+  };
+in
+{
+    imports = [
+      (import "${home-manager}/nixos")
+    ];
+
+    home-manager.users.henry = {
+      programs.firefox = {
+        enable = true;
+      };
+
+      programs.git = {
+        enable = true;
+        extraConfig = {
+          credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
+        };
+      };
+
+      home.file = {
+        ".mozzila/firefox/main/user.js".source = ./firefox/user.js;
+        # ".minecraft/options.txt".source = ./minecraft/options.txt;
+        # ".config/Code/User/settings.json".source = ./vscode/settings.json;
+      };
+    };
+}
