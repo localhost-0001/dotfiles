f7c859650f798b15f8aa221a94faac3b0aee8196
Henry Hoff <murp8752@gmail.com>
2021-08-17 07:55:18 -0400

    testing vscode with home-manager

--- home.nix
+++ home.nix
@@ -2,25 +2,44 @@
 let
   home-manager = builtins.fetchGit {
     url = "https://github.com/rycee/home-manager.git";
-    rev = "b39647e52ed3c0b989e9d5c965e598ae4c38d7ef"; # the commit to fetch 
+    rev = "b39647e52ed3c0b989e9d5c965e598ae4c38d7ef"; # the commit to fetch
     ref = "release-21.05"; # the branch to follow: release-xx.yy for stable nixos or master for nixos-unstable.
   };
 in
 {
-    imports = [
-      (import "${home-manager}/nixos")
-    ];
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
 
-    home-manager.users.henry = {
-      programs.firefox = {
-        enable = true;
-      };
+  home-manager.users.henry = {
+    programs.firefox = {
+      enable = true;
+    };
 
-      programs.git = {
-        enable = true;
-        extraConfig = {
-          credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
-        };
+    programs.git = {
+      enable = true;
+      extraConfig = {
+        credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
       };
     };
+
+    programs.vscode = {
+        enable = true;
+        package = pkgs.vscode;
+        extensions = with pkgs.vscode-extensions; [
+            bbenoist.Nix
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "rust";
+            publisher = "rust-lang";
+            version = "0.7.8";
+            sha256 = "039ns854v1k4jb9xqknrjkj8lf62nfcpfn0716ancmjc4f0xlzb3";
+          }
+        ];
+        userSettings = {
+            "terminal.integrated.fontFamily" = "Hack";
+        };
+    };
+  };
 }
