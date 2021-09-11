5233252c6fcdd8215c926945879b6b640e2287ab
poultryghast <murp8752@gmail.com>
2021-09-11 00:04:50 -0400

    made vscode beautiful

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -11,16 +11,28 @@
         sha256 = "039ns854v1k4jb9xqknrjkj8lf62nfcpfn0716ancmjc4f0xlzb3";
       }
       {
+        name = "org-mode";
+        publisher = "vscode-org-mode";
+        version = "1.0.0";
+        sha256 = "1dp6mz1rb8awrrpig1j8y6nyln0186gkmrflfr8hahaqr668il53";
+      }
+      {
         name = "even-better-toml";
         publisher = "tamasfe";
         version = "0.14.2";
         sha256 = "17djwa2bnjfga21nvyz8wwmgnjllr4a7nvrsqvzm02hzlpwaskcl";
       }
+      {
+        name = "vim";
+        publisher = "vscodevim";
+        version = "1.21.7";
+        sha256 = "160h8svp78snwq7bl6acbkmsb2664fiznnjqim9lh2bnyrlh69ww";
+      }
     ];
     userSettings = {
       "editor.minimap.enabled" = false;
       "editor.renderWhitespace" = "none";
-      "editor.fontSize" = 17;
+      "editor.fontSize" = 20;
       "workbench.editor.enablePreview" = false;
       "editor.wordWrap" = "on";
       "explorer.confirmDragAndDrop" = false;
@@ -29,6 +41,11 @@
       "javascript.updateImportsOnFileMove.enabled" = "always";
       "files.trimTrailingWhitespace" = true;
       "workbench.editor.untitled.hint" = "hidden";
+      "workbench.colorTheme" = "Default High Contrast";
+      "window.restoreFullscreen" = true;
+      "window.newWindowDimensions" = "maximized";
+      "editor.fontFamily" = "Ac437 IBM VGA 8x16";
+      "update.mode" = "none";
     };
   };
 }
