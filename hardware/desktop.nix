c85a484f92cae1dec9dcb2f88e19392238d20677
poultryghast <murp8752@gmail.com>
2021-09-12 12:24:06 -0400

    commit

--- hardware/desktop.nix
+++ hardware/desktop.nix
@@ -8,7 +8,7 @@ in
 
   config = mkIf cfg{
 		boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-	  boot.initrd.kernelModules = [ "amdgpu" ];
+	  boot.initrd.kernelModules = [ ]; #"amdgpu" ];
 	  boot.kernelModules = [ "kvm-amd" ];
 	  boot.extraModulePackages = [ ];
 
